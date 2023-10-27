c720fb4d408780d68d016d74267331e09b7f1fb1
NebihaDilsebo <shegolenur@hotmail.com>
2023-10-27 11:46:35 -0700

    test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -17,3 +17,13 @@ class TestAccessNestedMap(unittest.TestCase):
         """Access nested method
         """
         self.assertEqual(access_nested_map(nested_map, path), expected_result)
+
+    @parameterized.expand([
+        ({}, ("a",), "a not in nested_map"),
+        ({"a": 1}, ("a", "b"), "b not in {'a': 1}")
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, expected_error_message):
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map, path)
+
+        self.assertEqual(str(context.exception), expected_error_message)
