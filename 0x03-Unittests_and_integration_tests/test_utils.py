afe54de6a26d827d5ae670a1cb427f76bd68021c
NebihaDilsebo <shegolenur@hotmail.com>
2023-10-29 13:43:46 -0700

    test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,6 +3,7 @@
 """
 import unittest
 from unittest.mock import patch, Mock
+import requests
 from parameterized import parameterized
 from utils import access_nested_map
 from utils import get_json
